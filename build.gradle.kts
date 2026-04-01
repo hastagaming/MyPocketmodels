093cbd0f0559236e5f77bbcc476c42bef8f26404
hastagaming <rip.hastagaming@gmail.com>
2026-04-01 13:28:54 +0700

    Fix plugin repositories and add wrapper

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,6 @@
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    // Tambahkan baris di bawah ini jika kamu pakai KSP untuk Room Database
     id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false 
 }
